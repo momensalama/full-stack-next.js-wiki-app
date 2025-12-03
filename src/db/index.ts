4cc084ab9b5b36426645313feebe9db6a1d26e66
momen salama <momensalama712@gmail.com>
2025-12-03 13:07:33 +0200

    feat: ✨ add database schema and seeding functionality
    - Introduce Drizzle ORM for PostgreSQL integration.
    - Create articles table with necessary fields and constraints.
    - Implement seeding script to populate articles with sample data.

---
+++ src/db/index.ts
@@ -0,0 +1,14 @@
+import "dotenv/config";
+import assert from "node:assert";
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
+import * as schema from "./schema";
+
+assert(
+  process.env.DATABASE_URL,
+  "DATABASE_URL is not defined in environment variables"
+);
+
+export const sql = neon(process.env.DATABASE_URL);
+
+export const db = drizzle(sql, { schema });
